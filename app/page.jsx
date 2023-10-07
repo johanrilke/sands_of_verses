470ab3b659334d412a57e450efbdc39638773081
johanrilke <johanrilke.fe@gmail.com>
2023-10-08 01:59:33 +0700

    Create Form and Prompt Card SOV

--- app/page.jsx
+++ app/page.jsx
@@ -1,20 +1,18 @@
-import Feed from '@components/Feed';
+import Feed from "@components/Feed";
 
-const Home = () => {
-  return (
-    <section className="w-full flex-center flex-col">
-      <h1 className="head_text text-center">
-        Discover & share
-        <br className="max-md: hidden"/>
-      <span className="orange_gradient text-center"> AI Powered Prompts</span>
-      </h1>
-      <p className="desc text-center">
-        Promptopia is an open-source AI prompting tool for modern world to Discover and share creative prompts
-      </p>
+const Home = () => (
+  <section className='w-full flex-center flex-col'>
+    <h1 className='head_text text-center'>
+      Unearth the Sands of Time
+      <br className='max-md:hidden' />
+      <span className='orange_gradient text-center'>A Haven for Poetry</span>
+    </h1>
+    <p className='desc text-center'>
+    Dive into the world of verses with Sonorhyme, a platform that allows users to discover, create, and share poems and quotes that resonate with them. Share the resonance and connect with others through the power of words.
+    </p>
 
-      <Feed />
-    </section>
-  )
-}
+    <Feed />
+  </section>
+);
 
-export default Home
+export default Home;
